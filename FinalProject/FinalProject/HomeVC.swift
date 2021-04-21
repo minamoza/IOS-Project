9cfa00f8657169459978da717a590460f2244af9
minamoza <61341637+minamoza@users.noreply.github.com>
2021-04-22 00:55:47 +0600

    Add files via upload

---
+++ FinalProject/FinalProject/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  FinalProject
+//
+//  Created by Amina Moldamyrza on 4/22/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    @IBOutlet weak var mainCard: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+  
+
+}
