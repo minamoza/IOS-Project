a4b069bbeaf2da4490da88bb80cca3370a3df174
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-05-09 22:35:05 +0600

    upd

---
+++ FinalProject/FinalProject/Welcome/WelcomPageCell.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePageCell.swift
+//  FinalProject
+//
+//  Created by Zhaisan on 09.05.2021.
+//
+
+import UIKit
+
+class WelcomPageCell: UICollectionViewCell {
+        @IBOutlet weak var imageView: UIImageView!
+        @IBOutlet weak var titleLabel: UILabel!
+        @IBOutlet weak var descLabel: UILabel!
+
+        func setup(_ slide: Slide){
+            imageView.image = slide.image
+            titleLabel.text = slide.title
+            descLabel.text = slide.description
+        }
+}
